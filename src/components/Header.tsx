6a30cdca4bfb68878c95eb43856819f3dcc76985
alvaropsouza <alvaropatricio61@gmail.com>
2024-03-16 11:22:52 -0300

    feat: add improvements trough whole app

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,9 @@ import styles from './Header.module.css';
 export function Header() {
   return (
     <header className={styles.header}>
-      <img src={igniteTodoLogo} alt="Logotipo do Ignite" />
+      <a href="">
+        <img src={igniteTodoLogo} alt="Logotipo do Ignite" />
+      </a>
     </header>
   );
 }
